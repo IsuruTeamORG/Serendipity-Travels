adf313fd1357a9022bcf40a2c260552fc53a9fce
Isuru <isuru@cypsolabs.com>
2024-09-23 17:37:29 +0530

    create TourCards

---
+++ src/app/components/details/TourCardDetails.ts
@@ -0,0 +1,75 @@
+import img1 from "../../../../public/Image (3).png";
+import img2 from "../../../../public/Image (3).png";
+import img3 from "../../../../public/Image (3).png";
+import img4 from "../../../../public/Image (3).png";
+import img5 from "../../../../public/Image (3).png";
+import img6 from "../../../../public/Image (3).png";
+
+
+export const TourCardDetails = [
+  {
+    id: 1,
+    image: img1,
+    title: 'Cultural Heritage Tour',
+    duration: '8 Days / 7 Nights',
+    price: 900,
+    rating: 5
+  },
+  {
+    id: 2,
+    image: img2,
+    title: 'Beach Paradise Getaway',
+    duration: '5 Days / 4 Nights',
+    price: 650,
+    rating: 4
+  },
+  {
+    id: 3,
+    image: img3,
+    title: 'Mountain Trekking Adventure',
+    duration: '6 Days / 5 Nights',
+    price: 750,
+    rating: 4.5
+  },
+  {
+    id: 4,
+    image: img4,
+    title: 'City Explorer Package',
+    duration: '4 Days / 3 Nights',
+    price: 500,
+    rating: 4
+  },
+  {
+    id: 5,
+    image: img5,
+    title: 'Mountain Trekking Adventure',
+    duration: '6 Days / 5 Nights',
+    price: 750,
+    rating: 3
+  },
+  {
+    id: 6,
+    image: img6,
+    title: 'City Explorer Package',
+    duration: '4 Days / 3 Nights',
+    price: 500,
+    rating: 4
+  },
+  {
+    id: 5,
+    image: img5,
+    title: 'Mountain Trekking Adventure',
+    duration: '6 Days / 5 Nights',
+    price: 750,
+    rating: 2
+  },
+  {
+    id: 6,
+    image: img6,
+    title: 'City Explorer Package',
+    duration: '4 Days / 3 Nights',
+    price: 500,
+    rating: 3
+  }
+];
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Gallery from '../app/components/gallery'
+import { TourCardDetails } from './components/details/TourCardDetails'
+import TourCard from './components/TourCard'
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>page
+      <Gallery/>
+      <div>
+      <div className='mx-[5%] p-4'>
+        <div className="font-bold max-md:py-0 ">
+          <h2 className=" text-5xl">
+          Best Package For 2024
+          </h2>
+        </div>
+
+        <div className="pt-10 grid grid-cols-2  lg:grid-cols-4  gap-2 lg:gap-6 lg:mx-[5%] max-md:py-5">
+          {TourCardDetails.map((product: any) => {
+            return (
+              <div className="" key={product.id}>
+                <TourCard data={product} />
+              </div>
+            );
+          })}
+        </div>
+
+       
+      </div>
+      </div>
+    </div>
   )
 }
 
